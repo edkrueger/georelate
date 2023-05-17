9a80206d521af8a4fb1fed938f1e0ad2a4727e25
edkrueger <edkrueger@gmail.com>
2023-05-17 14:22:34 -0500

    Write and test distance_table.

--- georelate/_distance.py
+++ georelate/_distance.py
@@ -1,6 +1,8 @@
 """Computes distances."""
 
+# pylint:disable=too-many-arguments, too-many-locals
 import numpy as np
+import pandas as pd
 
 
 def haversine(p1_lat, p1_lon, p2_lat, p2_lon, radius=6367):
@@ -33,3 +35,86 @@ def haversine(p1_lat, p1_lon, p2_lat, p2_lon, radius=6367):
     d_r = 2 * np.arcsin(np.sqrt(partial))
 
     return radius * d_r
+
+
+def distance_table(
+    left,
+    right,
+    left_id=None,
+    right_id=None,
+    left_lat="lat",
+    left_lon="lon",
+    right_lat="lat",
+    right_lon="lon",
+    suffixes=("_left", "_right"),
+):
+    """_summary_
+
+    Args:
+        left (DataFrame): Left DataFrame to merge with. Assumes the index is an id.
+        right (DataFrame): Right DataFrame to merge with. Assumes the index is an id.
+        left_id (str, optional): Column containing the id for the left DataFrame.
+            If None is passed, assumes the index is the id.
+            Defaults to None.
+        right_id (str, optional): Column containing the id for the right DataFrame.
+            If None is passed, assumes the index is the id.
+            Defaults to None.
+        left_lat (str, optional): Column containing the latitude in the left DataFrame.
+            Defaults to "lat".
+        left_lon (str, optional): Column containing the longitude in the left DataFrame.
+            Defaults to "lon".
+        right_lat (str, optional): Column containing the latitude in the right DataFrame.
+            Defaults to "lat".
+        right_lon (str, optional): Column containing the longitude in the right DataFrame.
+            Defaults to "lon".
+        suffixes (tuple, optional): A length-2 sequence where each element is optionally a string
+            indicating the suffix to add to overlapping column names in left and right respectively.
+            Pass a value of None instead of a string to indicate that the column name from
+            left or right should be left as-is, with no suffix.
+            At least one of the values must not be None.
+            Defaults to ("_left", "_right").
+
+    Returns:
+        _type_: _description_
+    """
+
+    print(left)
+    print(right)
+
+    left_index_name = left.index.name if left.index.name else "index"
+    right_index_name = right.index.name if right.index.name else "index"
+
+    left_id_key = left_id if left_id else left_index_name
+    right_id_key = right_id if right_id else right_index_name
+
+    print(left_id_key)
+    print(right_id_key)
+
+    merged_df = pd.merge(
+        left=left.reset_index().loc[:, [left_id_key, left_lat, left_lon]],
+        right=right.reset_index().loc[:, [right_id_key, right_lat, right_lon]],
+        how="cross",
+        suffixes=suffixes,
+    )
+
+    left_lat_key = (
+        left_lat if left_lat in merged_df.columns else f"{left_lat}{suffixes[0]}"
+    )
+    left_lon_key = (
+        left_lon if left_lon in merged_df.columns else f"{left_lon}{suffixes[0]}"
+    )
+    right_lat_key = (
+        right_lat if right_lat in merged_df.columns else f"{right_lat}{suffixes[1]}"
+    )
+    right_lon_key = (
+        right_lon if right_lon in merged_df.columns else f"{right_lon}{suffixes[1]}"
+    )
+
+    return merged_df.assign(
+        distance=lambda df_: haversine(
+            p1_lat=df_[left_lat_key],
+            p1_lon=df_[left_lon_key],
+            p2_lat=df_[right_lat_key],
+            p2_lon=df_[right_lon_key],
+        )
+    )
--- tests/test__distance.py
+++ tests/test__distance.py
@@ -1,7 +1,9 @@
 """Tests _distance.py"""
 
 # pylint:disable=duplicate-code
-from georelate._distance import haversine
+import numpy as np
+import pandas as pd
+from georelate._distance import haversine, distance_table
 
 
 def test_haversine():
@@ -21,3 +23,197 @@ def test_haversine():
     result = haversine(p1_lat=p1_lat, p1_lon=p1_lon, p2_lat=p2_lat, p2_lon=p2_lon)
 
     assert result.round(0) == [distance]
+
+
+def test_distance_table_with_defaults():
+    """Tests the distance table function with defaults."""
+
+    # austin
+    austin_lat = [30.2672, 30.2673]
+    austin_lon = [97.7431, 97.7432]
+
+    # houston
+    houston_lat = [29.7604, 29.7605, 29.7606]
+    houston_lon = [95.3698, 95.3698, 95.3699]
+
+    austin_df = pd.DataFrame(
+        {
+            "lat": austin_lat,
+            "lon": austin_lon,
+        }
+    )
+
+    houston_df = pd.DataFrame(
+        {
+            "lat": houston_lat,
+            "lon": houston_lon,
+        }
+    )
+
+    result_df = distance_table(left=austin_df, right=houston_df)
+
+    assert result_df.shape[0] == austin_df.shape[0] * houston_df.shape[0]
+    assert "distance" in result_df.columns
+    assert result_df["distance"].dtype == np.float64
+    assert {
+        "lat_left",
+        "lon_left",
+        "lat_right",
+        "lon_right",
+        "index_left",
+        "index_right",
+    }.issubset(set(result_df.columns))
+
+    print(result_df)
+    print(result_df.dtypes)
+    print(result_df["distance"].dtype)
+    print(type(result_df["distance"].dtype))
+
+
+def test_distance_table_some_options_1():
+    """Tests the distance table function with some options."""
+
+    # austin
+    austin_id = [2, 5]
+    austin_lat = [30.2672, 30.2673]
+    austin_lon = [97.7431, 97.7432]
+
+    # houston
+    houston_id = [3, 6, 7]
+    houston_lat = [29.7604, 29.7605, 29.7606]
+    houston_lon = [95.3698, 95.3698, 95.3699]
+
+    austin_df = pd.DataFrame(
+        {
+            "austin_lat": austin_lat,
+            "austin_lon": austin_lon,
+        },
+        index=pd.Series(austin_id, name="austin_index"),
+    )
+
+    houston_df = pd.DataFrame(
+        {
+            "houston_lat": houston_lat,
+            "houston_lon": houston_lon,
+        },
+        index=pd.Series(houston_id, name="houston_index"),
+    )
+
+    result_df = distance_table(
+        left=austin_df,
+        right=houston_df,
+        left_lat="austin_lat",
+        left_lon="austin_lon",
+        right_lat="houston_lat",
+        right_lon="houston_lon",
+    )
+
+    assert result_df.shape[0] == austin_df.shape[0] * houston_df.shape[0]
+    assert "distance" in result_df.columns
+    assert result_df["distance"].dtype == np.float64
+    assert {
+        "austin_lat",
+        "austin_lon",
+        "houston_lat",
+        "houston_lon",
+        "austin_index",
+        "houston_index",
+    }.issubset(set(result_df.columns))
+
+
+def test_distance_table_some_options_2():
+    """Tests the distance table function with some options."""
+
+    # austin
+    austin_id = [2, 5]
+    austin_lat = [30.2672, 30.2673]
+    austin_lon = [97.7431, 97.7432]
+
+    # houston
+    houston_id = [3, 6, 7]
+    houston_lat = [29.7604, 29.7605, 29.7606]
+    houston_lon = [95.3698, 95.3698, 95.3699]
+
+    austin_df = pd.DataFrame(
+        {"austin_lat": austin_lat, "austin_lon": austin_lon, "austin_id": austin_id},
+    )
+
+    houston_df = pd.DataFrame(
+        {
+            "houston_lat": houston_lat,
+            "houston_lon": houston_lon,
+            "houston_id": houston_id,
+        },
+    )
+
+    result_df = distance_table(
+        left=austin_df,
+        right=houston_df,
+        left_id="austin_id",
+        right_id="houston_id",
+        left_lat="austin_lat",
+        left_lon="austin_lon",
+        right_lat="houston_lat",
+        right_lon="houston_lon",
+    )
+
+    assert result_df.shape[0] == austin_df.shape[0] * houston_df.shape[0]
+    assert "distance" in result_df.columns
+    assert result_df["distance"].dtype == np.float64
+    assert {
+        "austin_lat",
+        "austin_lon",
+        "houston_lat",
+        "houston_lon",
+        "austin_id",
+        "houston_id",
+    }.issubset(set(result_df.columns))
+
+
+def test_distance_table_some_options_3():
+    """Tests the distance table function with some options."""
+
+    # austin
+    austin_id = [2, 5]
+    austin_lat = [30.2672, 30.2673]
+    austin_lon = [97.7431, 97.7432]
+
+    # houston
+    houston_id = [3, 6, 7]
+    houston_lat = [29.7604, 29.7605, 29.7606]
+    houston_lon = [95.3698, 95.3698, 95.3699]
+
+    austin_df = pd.DataFrame(
+        {"lat": austin_lat, "lon": austin_lon, "id": austin_id},
+    )
+
+    houston_df = pd.DataFrame(
+        {
+            "lat": houston_lat,
+            "lon": houston_lon,
+            "id": houston_id,
+        },
+    )
+
+    result_df = distance_table(
+        left=austin_df,
+        right=houston_df,
+        left_id="id",
+        right_id="id",
+        left_lat="lat",
+        left_lon="lon",
+        right_lat="lat",
+        right_lon="lon",
+    )
+
+    assert result_df.shape[0] == austin_df.shape[0] * houston_df.shape[0]
+    assert "distance" in result_df.columns
+    assert result_df["distance"].dtype == np.float64
+    assert {
+        "lat_left",
+        "lon_left",
+        "lat_right",
+        "lon_right",
+        "id_left",
+        "id_right",
+    }.issubset(set(result_df.columns))
